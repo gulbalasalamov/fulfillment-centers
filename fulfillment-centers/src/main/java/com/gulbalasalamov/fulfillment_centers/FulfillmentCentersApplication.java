ad8735c7d8046fb5a14a13b44560c7ab44899e68
Gulbala Salamov <g.salamov@gmail.com>
2024-11-14 17:19:33 +0300

    chore(setup): project init

---
+++ fulfillment-centers/src/main/java/com/gulbalasalamov/fulfillment_centers/FulfillmentCentersApplication.java
@@ -0,0 +1,13 @@
+package com.gulbalasalamov.fulfillment_centers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulfillmentCentersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulfillmentCentersApplication.class, args);
+	}
+
+}
