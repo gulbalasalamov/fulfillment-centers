a7b4f8ff1daa2e24867f7684ab151af522607866
Gulbala Salamov <g.salamov@gmail.com>
2024-11-21 10:17:35 +0300

    feat: Add Swagger UI documentation using SpringDoc OpenAPI
    - Integrated SpringDoc OpenAPI for API documentation
    - Configured Swagger UI endpoint at /swagger-ui/index.html
    - Added Swagger configuration class for API info and contact details
    - Removed conflicting SpringFox dependencies to resolve application startup issues
    
    This commit enhances the project by adding a comprehensive API documentation tool, making it easier for developers to understand and interact with the API.

---
+++ fulfillment-centers/src/main/java/com/gulbalasalamov/fulfillment_centers/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.gulbalasalamov.fulfillment_centers.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Fulfillment Centers API")
+                        .description("API Endpoint Decoration")
+                        .version("v1.0")
+                        .contact(newContact())
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("API Documentation")
+                        .url("github.com/gulbalasalamov/fulfillment-centers"));
+    }
+
+    private Contact newContact(){
+        Contact contact = new Contact();
+        contact.setName("Gulbala Salamov");
+        contact.setUrl("https://www.github.com/gulbalasalamov");
+        contact.setEmail("g.salamov@gmail.com");
+        return contact;
+    }
+}
+
